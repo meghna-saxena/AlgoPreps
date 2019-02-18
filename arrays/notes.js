d93d9b1136e681276ed74b23650f75a8597ca07c
Meghna Srivastava <meghna.srivastava@auto1.com>
2019-02-18 17:50:12 +0100

    understanding arrays

---
+++ arrays/notes.js
@@ -0,0 +1,5 @@
+const notes = ['Note 1', 'Note 2', 'Note 3']
+
+console.log(`You have ${notes.length} notes`)
+console.log(`Last note: ${[notes.length - 1]}`)
+console.log(`Second last note: ${notes[notes.length - 2]}`)
